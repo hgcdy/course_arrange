f2bf1e81bb188b0f057727d9f44191e9a7d48b46
wjs <wjs@163.com>
2022-10-20 14:24:55 +0800

    v3_1 2022/10/20 6

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -2,10 +2,7 @@ require.config({
     paths: {
         jquery: '../jQuery3.6.0',
         util: '../util',
-        // bootstrap: 'https://cdn.staticfile.org/twitter-bootstrap/4.0.0/js/bootstrap.min',
-        // bootstrap: '../../bootstrap/js/bootstrap.min',
         bootstrapBundle: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min',
-        // popper: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min'
 
     },
     shim: {
