97f2eba070f4c1eb4bbe9ec02d7785d120921164
wjs <wjs@163.com>
2023-01-10 15:54:45 +0800

    v3_2 2022/1/10 7

--- src/main/resources/static/js/js/career.js
+++ src/main/resources/static/js/js/career.js
@@ -6,7 +6,7 @@ require(['../config'], function () {
             }
         });
         var college = null;
-        var careerId = null;
+        var careerIdList = "[]";
 
         query();
 
@@ -19,7 +19,22 @@ require(['../config'], function () {
                 success: function (data) {
                     if (data.code == 200) {
                         var $chunkElement = $(".chunk-card")[0];
-                        item($($chunkElement), data.data);
+                        var list = data.data;
+                        for (const item in list) {
+                            var $item = $("<div class='item'></div>").text(item).click(itemfun($(this).text()));
+                            var $img = $("<div class='img'></div>");
+                            var img2 = "<img src='../../img/alter.png' class='alter'>";
+                            $img.append(img2);
+                            $item.append($img);
+
+
+
+
+                        }
+
+
+
+
                     } else {
                         util.hint(data.msg)
                     }
@@ -28,12 +43,48 @@ require(['../config'], function () {
         }
 
 
-        function item(obj, list) {
-            for (const item in list) {
+
+        function itemfun(college) {
+            // $(this).
+            //todo college框起来
+            var $chunkElement = $(".chunk-card")[1];
+            $($chunkElement).empty();
+            $.ajax({
+                url: "nin-career/getCareerList",
+                dataType: "json",
+                type: "post",
+                data: {
+                    college: college
+                },
+                success: function (data) {
+                    if (data.code == 200) {
+                        var list = data.data;
+                        for (const item in list) {
+                            var $item = $("<div class='item'></div>").text(item).click(
+                                function () {
+                                    //todo 点击框起来
+                                }
+                            );
+                            var $img = $("<div class='img'></div>");
+                            var img1 = "<img src='../../img/query.png' class='query'>"; //查询，点击显示课程列表
+                            var img2 = "<img src='../../img/alter.png' class='alter'>"; //点击修改名称
+                            var img3 = "<img src='../../img/del1.png' class='del'>"; //删除
+
+                            $img.append(img1, img2, img3);
+                            $item.append($img);
+                            $chunkElement.append($item);
+
+                        }
+                    } else {
+                        util.hint(data.msg);
+                    }
+
+                }
+            });
 
 
 
-            }
         }
+
     })
 })
